47150f0b0c0d12530d7fd92c3d536a64f41da2b6
mdiktushar <md.tushar1804@gmail.com>
2025-12-11 18:40:15 +0600

    store data

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -2,11 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Book;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class CreateBook extends Component
 {
+    public $title;
+    public $author;
+    public $rating;
+
+
+    public function store()
+    {
+        Book::create([
+            'title' => $this->title,
+            'author' => $this->author,
+            'rating' => $this->rating,
+        ]);
+        $this->redirect('/');
+    }
+
     // #[Layout('components.layouts.second')]
     public function render()
     {
--- resources/views/livewire/create-book.blade.php
+++ resources/views/livewire/create-book.blade.php
@@ -1,3 +1,19 @@
 <div class="create">
     <h3>Create new book</h3>
+
+    <form wire:submit="store">
+        <div class="field">
+            <label for="title">Book Title</label>
+            <input type="text" name="" id="title" wire:model="title">
+        </div>
+        <div class="field">
+            <label for="author">Book Author</label>
+            <input type="text" name="" id="author" wire:model="author">
+        </div>
+        <div class="field">
+            <label for="rating">Book Rating</label>
+            <input type="text" name="" id="rating" wire:model="rating">
+        </div>
+        <Button>Add Book</Button>
+    </form>
 </div>
