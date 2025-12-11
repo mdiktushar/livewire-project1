6dea1ea8b19daa4502dfcd17a7b8b3df88d5f9f5
mdiktushar <md.tushar1804@gmail.com>
2025-12-11 18:45:25 +0600

    laravel validation in livewire

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -4,19 +4,29 @@ namespace App\Livewire;
 
 use App\Models\Book;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class CreateBook extends Component
 {
+    #[Rule('required|string|min:3|max:50')]
     public $title;
+    #[Rule('required|string|min:3|max:50')]
     public $author;
+    #[Rule('required|integer|min:1|max:10')]
     public $rating;
 
 
     public function store()
     {
+        $this->validate();
+        // $this->validate([
+        //     "title"  => "required|string|min:3|max:50",
+        //     "author" => "required|string|min:3|max:50",
+        //     "rating" => "required|integer|min:1|max:10",
+        // ]);
         Book::create([
-            'title' => $this->title,
+            'title'  => $this->title,
             'author' => $this->author,
             'rating' => $this->rating,
         ]);
--- resources/views/livewire/create-book.blade.php
+++ resources/views/livewire/create-book.blade.php
@@ -5,14 +5,23 @@
         <div class="field">
             <label for="title">Book Title</label>
             <input type="text" name="" id="title" wire:model="title">
+            @error('title')
+                <div class="error">{{ $message }}</div>
+            @enderror
         </div>
         <div class="field">
             <label for="author">Book Author</label>
             <input type="text" name="" id="author" wire:model="author">
+            @error('author')
+                <div class="error">{{ $message }}</div>
+            @enderror
         </div>
         <div class="field">
             <label for="rating">Book Rating</label>
             <input type="text" name="" id="rating" wire:model="rating">
+            @error('rating')
+                <div class="error">{{ $message }}</div>
+            @enderror
         </div>
         <Button>Add Book</Button>
     </form>
