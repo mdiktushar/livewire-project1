515f502154f986da6b45e26e2a7814cf6af959dd
mdiktushar <md.tushar1804@gmail.com>
2025-12-12 12:41:30 +0600

    modal store functin created

---
+++ app/Livewire/CreateModal.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Book;
+use Livewire\Component;
+
+class CreateModal extends Component
+{
+    public $title, $author, $rating;
+
+    protected $rules = [
+        "title"=> "required|string|min:3|max:50",
+        "author"=> "required|string|min:3|max:50",
+        "rating"=> "required|integer|min:1|max:10",
+    ];
+
+    public function submit()
+    {
+        $this->validate();
+
+        Book::create([
+            'title'  => $this->title,
+            'author' => $this->author,
+            'rating' => $this->rating,
+        ]);
+    }
+
+    public function render()
+    {
+        return view('livewire.create-modal');
+    }
+}
