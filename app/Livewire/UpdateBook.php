5c983b236f344b46ddaea67afe9db6b1520c7ac7
mdiktushar <md.tushar1804@gmail.com>
2025-12-12 16:08:23 +0600

    update data fetch

--- app/Livewire/UpdateBook.php
+++ app/Livewire/UpdateBook.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Book;
 use Livewire\Component;
 
 class UpdateBook extends Component
 {
+    public Book $book;
+
+    public function mount(Book $book)
+    {
+        $this->book = $book;
+    }
+
     public function render()
     {
-        return view('livewire.update-book');
+        return view('livewire.update-book', ['book' => $this->book]);
     }
 }
