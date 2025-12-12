059de969b7dc3987e65a2c66b6d529cace0e1815
mdiktushar <md.tushar1804@gmail.com>
2025-12-12 13:07:27 +0600

    flux install\

---
+++ resources/views/livewire/create-modal.blade.php
@@ -0,0 +1,3 @@
+<div class="create">
+asdfdsa fgdsg asdfa fasd f
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Livewire\BookList;
 use App\Livewire\CreateBook;
+use App\Livewire\CreateModal;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', BookList::class);
 Route::get('/create', CreateBook::class);
+Route::get('/create-modal', CreateModal::class);
